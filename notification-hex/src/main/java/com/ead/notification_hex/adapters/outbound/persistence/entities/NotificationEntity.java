ceb9ce6b206da23abe020956f47d2d3f9049287d
Igorgcf <igorTecnologia@outlook.com>
2026-05-15 15:47:15 -0300

    Notification Entity

---
+++ notification-hex/src/main/java/com/ead/notification_hex/adapters/outbound/persistence/entities/NotificationEntity.java
@@ -0,0 +1,42 @@
+package com.ead.notification_hex.adapters.outbound.persistence.entities;
+
+import com.ead.notification_hex.core.domain.enums.NotificationStatus;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "tb_notification")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class NotificationEntity implements Serializable {
+    private static final long SerialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @Column(nullable = false)
+    private UUID userId;
+
+    @Column(nullable = false, length = 150)
+    private String title;
+
+    @Column(nullable = false)
+    private String message;
+
+    @Column(nullable = false)
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'")
+    private LocalDateTime creationDate;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private NotificationStatus notificationStatus;
+}
